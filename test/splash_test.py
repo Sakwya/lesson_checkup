678e9479b97871a09b921848ea6ad056943eb560
Skuyazlu <celiasash@foxmail.com>
2024-04-11 11:34:20 +0800

    111

--- test/splash_test.py
+++ test/splash_test.py
@@ -1,25 +1,23 @@
 import requests
 from PIL import Image
 from io import BytesIO
+import matplotlib.pyplot as plt
+
+# 如果lua代码中有特殊字符，要通过进行quote转码
+from urllib.parse import quote
 
 
 if __name__ == '__main__':
-    url = 'https://www.google.com.hk/'
-    payload = {
-        'url': url,
-        'render_all': True,  # 渲染整个页面
-        'width': 1280,  # 截图宽度
-        'height': 800,  # 截图高度
-        'timeout': 30,  # 页面加载超时时间
-        'viewport': '1280x800',  # 视口大小
-        'format': 'png',  # 截图格式
-    }
-
-    response = requests.post('http://127.0.0.1:8050/render.png', json=payload)
+    url = 'https://sso.ecust.edu.cn/authserver/login?service=http://s.ecust.edu.cn/sso/ecustsb'
+    with open("main.lua", "r") as f:
+        lua = f.read()
+    base_url = f'http://localhost:8050/execute?url={url}&lua_source={quote(lua)}'
+    response = requests.get(base_url)
     if response.status_code == 200:
         # 从二进制数据中创建图像
         img = Image.open(BytesIO(response.content))
         # 显示图像
-        img.show()
+        plt.imshow(img)
+        plt.show()
     else:
-        print("截图请求失败，状态码：", response.status_code)
+        print("截图请求失败", response.text)
